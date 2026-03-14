0fa9a84c17b626f1173146a2e4c0f9b8b8a6176e
astn <git@mail.astn.io>
2026-03-13 22:17:07 -0700

    Added a cute transition for the toggle icon

--- src/components/LightToggle.svelte
+++ src/components/LightToggle.svelte
@@ -35,8 +35,13 @@
   onclick={toggleTheme}
   title={`Toggle ${currentMode === 'light' ? 'Dark' : 'Light'} Mode`}
 >
-  <span class="color-scheme-toggle--icon ri-sun-fill" data-active="true"></span>
-  <span class="color-scheme-toggle--icon ri-moon-clear-fill" data-active="false"
+  <span
+    class="icon icon-light ri-sun-fill"
+    data-active={currentMode === 'light' ? 'true' : 'false'}
+  ></span>
+  <span
+    class="icon icon-dark ri-moon-clear-fill"
+    data-active={currentMode === 'dark' ? 'true' : 'false'}
   ></span>
 </button>
 
@@ -56,15 +61,23 @@
     border: none;
   }
 
-  .color-scheme-toggle--icon {
+  .icon {
+    position: absolute;
+
     font-size: 1.2rem;
     transition:
       opacity 0.3s ease,
       transform 0.3s ease;
   }
 
+  [data-active='true'] {
+    transform: scale(1, 1);
+    opacity: 1;
+  }
+
   [data-active='false'] {
-    display: none;
+    transform: scale(0, 0);
+    opacity: 0;
   }
 
   @keyframes colorSchemeTransition {
@@ -73,6 +86,7 @@
     }
     to {
       scale: (0, 1);
+      display: none;
     }
   }
 </style>
