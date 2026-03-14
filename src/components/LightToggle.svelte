b72b08c77ea25b09aaca0e9eb93b3f83027bdb73
astn <git@mail.astn.io>
2026-03-13 19:45:01 -0700

    Fixed issue with the incorrect light mode being applied

--- src/components/LightToggle.svelte
+++ src/components/LightToggle.svelte
@@ -1,31 +1,41 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  let prefersLightMode: boolean = false;
+  let currentMode = $state('Light');
 
   function toggleTheme() {
-    if (prefersLightMode) {
+    if (localStorage.getItem('colorScheme') === 'light') {
       document.documentElement!.dataset.colorScheme = 'dark';
-      prefersLightMode = false;
-    } else if (!prefersLightMode) {
+      localStorage.setItem('colorScheme', 'dark');
+      currentMode = 'Dark';
+    } else if (localStorage.getItem('colorScheme') === 'dark') {
       document.documentElement!.dataset.colorScheme = 'light';
-      prefersLightMode = true;
+      localStorage.setItem('colorScheme', 'light');
+      currentMode = 'Light';
     }
   }
 
   onMount(() => {
-    const prefersLightMode = window.matchMedia(
-      '(prefers-color-scheme: light)',
-    ).matches;
+    let prefersLightMode: boolean = false;
+
+    currentMode =
+      localStorage.getItem('colorScheme') === 'dark' ? 'Dark' : 'Light';
+
+    if (localStorage.getItem('colorScheme')) {
+      prefersLightMode =
+        localStorage.getItem('colorScheme') === 'light' ? true : false;
+    } else {
+      prefersLightMode = window.matchMedia(
+        '(prefers-color-scheme: light)',
+      ).matches;
+    }
 
     if (prefersLightMode) {
       document.documentElement!.dataset.colorScheme = 'light';
-    }
-
-    if (!prefersLightMode) {
+    } else if (!prefersLightMode) {
       document.documentElement!.dataset.colorScheme = 'dark';
     }
   });
 </script>
 
-<button onclick={toggleTheme}> Toggle Light Mode </button>
+<button onclick={toggleTheme}> Toggle {currentMode} Mode </button>
