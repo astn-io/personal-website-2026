2727d9dba84818828736c2de85d6c51f34c45fa0
astn <git@mail.astn.io>
2026-03-15 12:58:39 -0700

    Removed defined animations in the Base and moved them to the transitions.scss. Adjusted timing of animations. Changed the animations used to just be a fade for a more subtle and user-friendly effect

--- src/scripts/custom-transitions/slideIn.ts
+++ src/scripts/custom-transitions/slideIn.ts
@@ -1,4 +1,9 @@
-export const slideInAnim = {
+/**
+ * > [!NOTE]
+ * > Animations are found in /src/styles/transitions.scss
+ */
+
+export const slideAnim = {
   old: {
     name: 'slide-out',
     duration: '0.2s',
@@ -11,7 +16,25 @@ export const slideInAnim = {
   },
 };
 
-export const slideInTransition = {
-  forwards: slideInAnim,
-  backwards: slideInAnim,
+export const slideTransition = {
+  forwards: slideAnim,
+  backwards: slideAnim,
+};
+
+export const fadeAnim = {
+  old: {
+    name: 'fade-out',
+    duration: '200ms',
+    easing: 'ease-out',
+  },
+  new: {
+    name: 'fade-in',
+    duration: '200ms',
+    easing: 'ease-out',
+  },
+};
+
+export const fadeTransition = {
+  forwards: fadeAnim,
+  backwards: fadeAnim,
 };
