f1f3a46a69a490ff95ad913eef7cd5b8497a1e11
astn <git@mail.astn.io>
2026-03-15 11:40:51 -0700

    Fixed issue with AppBar going below content during transition. Refactored custom transition to be in its own file and directory

---
+++ src/scripts/custom-transitions/slideIn.ts
@@ -0,0 +1,17 @@
+export const slideInAnim = {
+  old: {
+    name: 'slide-out',
+    duration: '0.2s',
+    easing: 'ease-out',
+  },
+  new: {
+    name: 'slide-in',
+    duration: '0.2s',
+    easing: 'ease-out',
+  },
+};
+
+export const slideInTransition = {
+  forwards: slideInAnim,
+  backwards: slideInAnim,
+};
