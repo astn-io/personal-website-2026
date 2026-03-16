df41722ae1fc8a772a59654d8123798897a84c08
astn <git@mail.astn.io>
2026-03-16 12:41:19 -0700

    Inserted the list of navigation links in a nav element to be more semantic

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,17 +1,19 @@
-<ul>
-  <li>
-    <a href="/">Home</a>
-  </li>
-  <li>
-    <a href="/about">About</a>
-  </li>
-  <li>
-    <a href="/blog">Blog</a>
-  </li>
-  <li>
-    <a href="/projects">Projects</a>
-  </li>
-</ul>
+<nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/about">About</a>
+    </li>
+    <li>
+      <a href="/blog">Blog</a>
+    </li>
+    <li>
+      <a href="/projects">Projects</a>
+    </li>
+  </ul>
+</nav>
 
 <style>
   ul {
