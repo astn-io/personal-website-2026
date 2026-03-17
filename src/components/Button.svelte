6af562ebf0b6be74f76a06e7316cb6d7e2785fd8
astn <git@mail.astn.io>
2026-03-17 09:57:01 -0700

    Created a Button component used for both links and buttons with onclick events

---
+++ src/components/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { MouseEventHandler } from 'svelte/elements';
+
+  type Props = {
+    label: string;
+    link?: string;
+    icon?: string;
+    onclick?: MouseEventHandler<HTMLButtonElement>;
+  };
+
+  const { label, link, icon, onclick }: Props = $props();
+</script>
+
+{#if link}
+  <a href={link}>
+    <span class={`${icon}`}></span>
+    <span>{label}</span>
+  </a>
+{:else}
+  <button {onclick}>
+    <span class={`${icon}`}></span>
+    <span>{label}</span>
+  </button>
+{/if}
