f23153e46dab4ab5bff40d324d84e2f8bf1d83ff
astn <git@mail.astn.io>
2026-03-17 11:07:49 -0700

    Added some additional props, styling, and variants to buttons, as well as adding an example of them to the hero

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,19 +6,48 @@
     link?: string;
     icon?: string;
     onclick?: MouseEventHandler<HTMLButtonElement>;
+    variant?: 'primary' | 'secondary' | 'tertiary';
   };
 
-  const { label, link, icon, onclick }: Props = $props();
+  const { label, link, icon, onclick, variant = 'primary' }: Props = $props();
 </script>
 
 {#if link}
-  <a href={link}>
+  <a class={`btn btn-${variant}`} href={link}>
     <span class={`${icon}`}></span>
     <span>{label}</span>
   </a>
 {:else}
-  <button {onclick}>
+  <button class={`btn btn-${variant}`} {onclick}>
     <span class={`${icon}`}></span>
     <span>{label}</span>
   </button>
 {/if}
+
+<style>
+  a.btn,
+  button.btn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: none;
+
+    padding-inline: 1rem;
+    padding-block: 0.6rem;
+
+    cursor: pointer;
+  }
+
+  .btn-primary {
+    background-color: var(--clr-primary);
+  }
+
+  .btn-secondary {
+    background-color: var(--clr-secondary);
+  }
+
+  .btn-tertiary {
+    background-color: var(--clr-tertiary);
+  }
+</style>
