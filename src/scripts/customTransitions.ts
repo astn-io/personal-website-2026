cf011dc0ca72b8afa96d4865ff55d3ee44ab2e0f
astn <git@mail.astn.io>
2026-03-30 23:01:42 -0700

    Refactored the CardImage so that the transforms are on the picture/image itself and not on the container element, allowing more flexibility in the future depending on the container element.  Adjusted some timings on transitions and touched up notes for them.

--- src/scripts/customTransitions.ts
+++ src/scripts/customTransitions.ts
@@ -1,17 +1,17 @@
 /**
- * > [!NOTE]
- * > Animations are found in /src/styles/transitions.scss
+ * NOTE:
+ * Animations are found in ./src/styles/transitions.scss
  */
 
 export const slideAnim = {
   old: {
     name: 'slide-out',
-    duration: '0.2s',
+    duration: '350ms',
     easing: 'ease-out',
   },
   new: {
     name: 'slide-in',
-    duration: '0.2s',
+    duration: '350ms',
     easing: 'ease-out',
   },
 };
@@ -24,12 +24,12 @@ export const slideTransition = {
 export const fadeAnim = {
   old: {
     name: 'fade-out',
-    duration: '200ms',
+    duration: '350ms',
     easing: 'ease-out',
   },
   new: {
     name: 'fade-in',
-    duration: '200ms',
+    duration: '350ms',
     easing: 'ease-out',
   },
 };
