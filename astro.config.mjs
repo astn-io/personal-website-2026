225ec7b4e2bdcab56122fba4aa5bf74381a0495c
astn <git@mail.astn.io>
2026-04-13 12:35:27 -0700

    Added custom Shiki theme for code blocks in posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import astnShikiTheme from './src/styles/shiki-theme.json' with { type: 'json' };
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astn.io',
   integrations: [svelte()],
   image: { layout: 'constrained' },
+  markdown: {
+    shikiConfig: {
+      theme: astnShikiTheme,
+      wrap: false,
+    },
+  },
 });
