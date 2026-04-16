f4ff79be26a54b4896de16f9023f45a8e1da76d3
astn <git@mail.astn.io>
2026-04-16 12:48:33 -0700

    Refactored project posts and blog posts to use a shared layout, and most information was shared between them anyway. Fixed a type issue with shiki. Using the site property from the astro.config.mjs file to determine the site url instead of manually entering it now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,14 @@ import astnShikiTheme from './src/styles/shiki-theme.json' with { type: 'json' }
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astn.io',
+  site: 'https://test.astn.io',
   integrations: [svelte()],
   image: { layout: 'constrained' },
   markdown: {
     shikiConfig: {
-      theme: astnShikiTheme,
+      theme: /** @type {import('shiki').ThemeRegistration} */ (
+        /** @type {unknown} */ (astnShikiTheme)
+      ),
       wrap: false,
     },
   },
