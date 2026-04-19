55a48c61bde4299083fda46d9da90c1e5282197c
astn <git@mail.astn.io>
2026-04-19 14:20:59 -0700

    Setup Astro Node adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,11 +5,18 @@ import svelte from '@astrojs/svelte';
 
 import astnShikiTheme from './src/styles/shiki-theme.json' with { type: 'json' };
 
+import node from '@astrojs/node';
+
+import { loadEnv } from 'payload/node';
+
+loadEnv();
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://test.astn.io',
   integrations: [svelte()],
   image: { layout: 'constrained' },
+
   markdown: {
     shikiConfig: {
       theme: /** @type {import('shiki').ThemeRegistration} */ (
@@ -18,4 +25,8 @@ export default defineConfig({
       wrap: false,
     },
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
